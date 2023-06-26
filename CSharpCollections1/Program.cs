9bad39c864f46454147bc26ee1800e62c9c3e29d
9inebar <9inebar@gmail.com>
2023-06-27 00:30:52 +0300

    CSharpCollections1

---
+++ CSharpCollections1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+int[] evenNumbers = new int[5];
+int[] oddNumbers = new int[5];
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = i; j < evenNumbers.Length; j++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            evenNumbers[j] = numbers[i];
+        }
+    }
+    for (int j = i; j < oddNumbers.Length; j++)
+    {
+        if (numbers[i] % 2 != 0)
+        {
+            oddNumbers[j] = numbers[i];
+        }
+    }
+}
+
+foreach (int i in evenNumbers)
+{
+    Console.WriteLine(i);
+}
